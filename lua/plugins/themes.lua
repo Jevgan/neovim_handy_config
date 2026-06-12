11cceb8e48a0c9c0463a88fb8a21a51d813c5344
Jevgan <skinnycoding@gmail.com>
2026-06-12 21:50:21 +0300

    fix: resolve visual inconsistency in theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -6,25 +6,23 @@ return {
             require('rose-pine').setup({
                 variant = 'auto', -- 'main', 'moon', or 'dawn'
                 dark_variant = 'main',
-                dim_inactive_windows = false,
+                dim_inactive_windows = true,
                 -- extend_background_behind_borders = true,
 
                 styles = {
                     bold = true,
                     italic = false,
-                    transparency = true, -- <--- THIS IS THE KEY SETTING
                 },
 
                 highlight_groups = {
-                    background = { bg = "NONE" },
                     fill = { bg = "NONE" },
                 },
             })
 
             vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
-            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
-            vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
-            vim.api.nvim_set_hl(0, "Pmenu", { bg = "NONE" })
+            -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+            -- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE" })
+            -- vim.api.nvim_set_hl(0, "Pmenu", { bg = "NONE" })
 
             -- Also target non-text areas
             vim.api.nvim_set_hl(0, "LineNr", { bg = "NONE" })
